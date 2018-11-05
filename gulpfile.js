1eecb9e67dcfc19831709410cb9c6d320391ee01
yokselzok <yokselzok@gmail.com>
2018-11-06 00:22:52 +0300

    Make publish script more useful

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,34 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var ghPages = require('gulp-gh-pages');
 var colors = require('colors/safe');
+var copy = require('gulp-copy');
+var del = require('del');
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
+
+// CLEAN BUILD
+gulp.task('clean', function(){
+  del(['build/*']).then(paths => {
+    console.log('⬤  Deleted files and folders:\n', paths.join('\n'));
+  });
+});
+
+gulp.task('make-index', function(){
+  console.log(colors.yellow('⬤  Make index page... ⬤'));
+
+  return gulp.src(['index-src.html'])
+    .pipe(replace('<!-- content -->', '<a href="demo/dancing-shadows.html">dancing-shadows</a>'))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('build/'));
+});
+
+// CLEAN BUILD & COPY FILES TO IT
+gulp.task('copy', function() {
+  console.log(colors.magenta('⬤  Copy files to build... ⬤'));
+
+  return gulp.src(['assets/**/*','demo/**/*'])
+    .pipe(copy('build/'));
+});
 
 // WATCH HTML, PREPROCESS AND RELOAD
 gulp.task('serve', function() {
@@ -16,7 +44,7 @@ gulp.task('serve', function() {
 });
 
 // PUBLISH TO GITHUB PAGES
-gulp.task('ghPages', function() {
+gulp.task('ghPages', ['clean','copy', 'make-index'],function() {
   console.log(colors.rainbow('⬤  Publish to Github Pages... ⬤'));
 
   return gulp.src('build/**/*')
